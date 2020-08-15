4cf59481a7bace5b376440280df62307212fb7e2
Ekaterina Kurochka <ekaterina1.kur@gmail.com>
2020-08-15 18:46:18 +0300

    Updates

---
+++ precache-manifest.3adf68c4fa9e9df2b7eb32fdcfc04a99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01613299bfc2401690a3b0a95580bf39",
+    "url": "/youtube-api/index.html"
+  },
+  {
+    "revision": "c89f2e32e8f78ad436b4",
+    "url": "/youtube-api/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4e8ca3eb3dfaa6041b16",
+    "url": "/youtube-api/static/js/2.511c948c.chunk.js"
+  },
+  {
+    "revision": "943aec86c35bb3f39d18124af76a452f",
+    "url": "/youtube-api/static/js/2.511c948c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c89f2e32e8f78ad436b4",
+    "url": "/youtube-api/static/js/main.502b33a0.chunk.js"
+  },
+  {
+    "revision": "31c4f16a846b4c41fddc",
+    "url": "/youtube-api/static/js/runtime-main.253513a9.js"
+  }
+]);
